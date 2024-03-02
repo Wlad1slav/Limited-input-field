07f3ae20ec651a079e90a356e070ddb984023489
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-03-02 14:29:37 +0200

    added check for minimum characters

--- js/limited-input-field-scripts.js
+++ js/limited-input-field-scripts.js
@@ -8,15 +8,21 @@ for (let i = 0; i < fieldContainers.length; i++) {
 
     // set standard amount of characters at field & max length
     fieldContainers[i].getElementsByClassName('characters-amount')[0].innerHTML
-        = `${field.value.length}/${field.max}`;
+        = `${field.value.length-field.min}/${field.max-field.min}`;
 
     field.addEventListener('input', () => {
         // update the characters amount paragraph
         // set field value length
         const p = fieldContainers[i].getElementsByClassName('characters-amount')[0];
-        p.innerHTML = `${field.value.length}/${field.max}`;
+        p.innerHTML = `${field.value.length-field.min}/${field.max-field.min}`;
 
-        if (field.value.length > field.max) {
+        if (field.value.length-field.min < 0) {
+            // if is no minimum number of characters in the input field
+            field.classList.add('warning');
+            p.classList.add('warning');
+        }
+
+        else if (field.value.length > field.max) {
             // if the limit of characters was exceeded
             field.classList.add('error');
             p.classList.add('error');
@@ -27,6 +33,13 @@ for (let i = 0; i < fieldContainers.length; i++) {
             // but no longer exceeds of length
             field.classList.remove('error');
             p.classList.remove('error');
+        }
+
+        else if (field.classList.contains('warning') && field.value.length-field.min >= 0) {
+            // if the input field has an warning class
+            // but characters enough already
+            field.classList.remove('warning');
+            p.classList.remove('warning');
         }
     })
 }
